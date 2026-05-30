e42442ca282ad5e87c1e4045c1dde43e7d2a9a48
dddevid <141579210+dddevid@users.noreply.github.com>
2026-05-30 14:01:20 +0200

    Add Azerbaijani (az) to supported locales list

--- lib/l10n/app_localizations.dart
+++ lib/l10n/app_localizations.dart
@@ -119,6 +119,7 @@ abstract class AppLocalizations {
   /// A list of this localizations delegate's supported locales.
   static const List<Locale> supportedLocales = <Locale>[
     Locale('ar'),
+    Locale('az'),
     Locale('bn'),
     Locale('da'),
     Locale('de'),
